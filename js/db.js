d541bc2bfeb7217614b181d53d8dec7f72ae9ca3
helheim.k <helheimkika@hotmail.com>
2026-06-08 21:30:05 -0600

    iniciamos mostrar platillos en la aplicacion

--- js/db.js
+++ js/db.js
@@ -1,5 +1,5 @@
 db.collection("platillos").onSnapshot((coleccion) => {
     coleccion.forEach((registro) => {
-    console.log(registro);
+    mostrarPlatillo(registro.data(), registro.id);
     });
 });
